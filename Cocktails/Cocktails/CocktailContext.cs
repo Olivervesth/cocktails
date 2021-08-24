b2d6e893f508b7db3299e5b7dbebbebb493f1124
Olivervesth <48321388+Olivervesth@users.noreply.github.com>
2021-08-24 22:19:40 +0200

    Add files via upload

---
+++ Cocktails/Cocktails/CocktailContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocktails
+{
+    class CocktailContext : DbContext
+    {
+        public CocktailContext() : base()
+        {
+            //Database.SetInitializer<CocktailContext>(new CreateDatabaseIfNotExists<CocktailContext>());
+            Database.SetInitializer<CocktailContext>(new DropCreateDatabaseAlways<CocktailContext>());
+        }
+        public DbSet<Cocktail> Cocktails { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
